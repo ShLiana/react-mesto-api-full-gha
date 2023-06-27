ee336d5432fbbde805e0613786e8925bb6e8b44c
Liana Shmeleva <sayapovaliana@yandex.ru>
2023-06-27 15:11:47 +0500

    изменяет адрес подключения к бд

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const NotFound = require('./utils/errors/NotFound');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
