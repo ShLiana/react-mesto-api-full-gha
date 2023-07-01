9cab026e015b157319affa4679ea02cbe4a7c5ac
Liana Shmeleva <sayapovaliana@yandex.ru>
2023-07-02 01:58:59 +0500

    new123

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,7 +1,7 @@
 class Api {
   constructor(options) {
     this._url = options.url;
-    this._headers = options.headers;
+    //this._headers = options.headers;
   }
 
   //проверить ответ сервера
@@ -12,25 +12,24 @@ class Api {
       return Promise.reject(`Ошибка: ${res.status}`);
   }
 
-  _getHeaders() {
-    const jwt = localStorage.getItem('jwt');
-    return {
-      'Authorization': `Bearer ${jwt}`,
-      ...this._headers,
-    };
-  }
-
   //получить карточки
   getInitialCards() {
+    const token = localStorage.getItem('jwt');
     return fetch(`${this._url}/cards`, {
-      headers: this._getHeaders,
+      method: "GET",
+      headers: {
+      authorization: `Bearer ${token}`
+      }
     }).then((res) => this._getJson(res));
   }
 
   //получить информацию о пользователе
   getUserInfo() {
+    const token = localStorage.getItem('jwt');
     return fetch(`${this._url}/users/me`, {
-      headers: this._getHeaders,
+      headers: {
+        authorization: `Bearer ${token}`
+      }
     }).then((res) => this._getJson(res));
   }
 
@@ -38,7 +37,7 @@ class Api {
   updateUserInfo(data) {
     return fetch(`${this._url}/users/me`, {
       method: "PATCH",
-      headers: this._getHeaders,
+      headers: this._headers,
       body: JSON.stringify({
         name: data.userName,
         about: data.userJob,
@@ -50,7 +49,7 @@ class Api {
   updateUserAvatar(data) {
     return fetch(`${this._url}/users/me/avatar`, {
       method: "PATCH",
-      headers: this._getHeaders,
+      headers: this._headers,
       body: JSON.stringify({
         avatar: data.userAvatar,
       }),
@@ -61,7 +60,7 @@ class Api {
   addNewCard(data) {
     return fetch(`${this._url}/cards`, {
       method: "POST",
-      headers: this._getHeaders,
+      headers: this._headers,
       body: JSON.stringify({
         name: data.name,
         link: data.link,
@@ -73,7 +72,7 @@ class Api {
   deleteCard(_id) {
     return fetch(`${this._url}/cards/${_id}`, {
       method: "DELETE",
-      headers: this._getHeaders,
+      headers: this._headers,
     }).then((res) => this._getJson(res));
   }
 
@@ -91,12 +90,12 @@ class Api {
     if (isLiked) {
       return fetch(`${this._url}/cards/${_id}/likes`, {
         method: "PUT",
-        headers: this._getHeaders,
+        headers: this._headers,
       }).then((res) => this._getJson(res));
     } else {
       return fetch(`${this._url}/cards/${_id}/likes`, {
         method: "DELETE",
-        headers: this._getHeaders,
+        headers: this._headers,
       }).then((res) => this._getJson(res));
     }
   }
@@ -114,6 +113,7 @@ class Api {
 const api = new Api({
   url: "https://api.shmeleva.nomoreparties.sbs",
   headers: {
+    Authorization: "",
     "Content-Type": "application/json",
   },
 });
